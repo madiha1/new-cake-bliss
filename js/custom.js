67c7179adfdd5d59d67ea5ac201baf9840e1088f
waleed <143600298+MWaleed0322@users.noreply.github.com>
2026-01-05 18:03:23 +0500

    create inner pages

--- js/custom.js
+++ js/custom.js
@@ -192,3 +192,23 @@ $('.menu-slider-btn-left').on('click', function () {
 $('.menu-slider-btn-right').on('click', function () {
     $('.menuSlide').slick('slickNext');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cheese = document.querySelector('.cheese');
+
+    let deg = 0;
+
+    function spin() {
+        setInterval(() => {
+            deg++;
+            if (deg >= 360) {
+                deg = 0;
+            } else {
+                cheese.style.transform = `rotate(${deg}deg)`;
+            }
+        }, 50);
+    }
+
+    spin();
+});
